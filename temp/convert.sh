23dca380000d1de7c450c069bad7f3660cf5d956
Rajko Zschiegner <ricki_z@gmx.de>
2016-05-16 00:26:17 +0200

    old file

--- temp/convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-while read LINE; do
-    ts=$(date -d "`echo $LINE | awk '{print $1, $2, $3, $4, $5, $6 }' | sed s/;//`" +%s)
-+%s | sed -r s/$/:/
-    temp=`echo "$LINE" | awk '{print $7}'`
-    echo "rrdtool update temps_take_2.rrd ${ts}:${temp}"
-done < 2015-12-21_ppd42ns_sensor_36.csv
