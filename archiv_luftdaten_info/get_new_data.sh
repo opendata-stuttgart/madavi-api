d0e5c4bdb53f5097d92f85fb93020d7ed20d2c44
Rajko Zschiegner <ricki_z@gmx.de>
2016-05-15 18:24:50 +0200

    script for crontab added.

---
+++ archiv_luftdaten_info/get_new_data.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export DATE=`/bin/date -d yesterday +%F`
+export WORKPATH=$(dirname $(readlink -f $0))
+
+echo Datum: $DATE
+echo $WORKPATH
+cd $WORKPATH/mirror
+host archive.luftdaten.info
+sleep 10
+wget --mirror -N -np -q http://archive.luftdaten.info/$DATE/
+cd $WORKPATH
+./update.pl $DATE
