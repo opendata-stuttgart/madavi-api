617dd0a6a3fd3af116a39cbddc109b28ef1f4c31
Rajko Zschiegner <ricki_z@gmx.de>
2016-05-16 18:42:39 +0200

    cleanup of rrdgraph options

--- data.php
+++ data.php
@@ -8,91 +8,76 @@ $results = json_decode($json,true);
 $now = gmstrftime("%Y/%m/%d %H:%M:%S");
 $today = gmstrftime("%Y-%m-%d");
 
+// copy sensor data values to values array
 foreach ($results["sensordatavalues"] as $sensordatavalues) {
 	$values[$sensordatavalues["value_type"]] = $sensordatavalues["value"];
 }
 
+// check for used sensors
+if (isset($values["durP1"]) && isset($values["durP2"])) { $has_ppd42ns = 1; } else { $has_ppd42ns = 0; }
+if ((! isset($values["durP1"])) && isset($values["P1"])) { $has_sds011 = 1; } else { $has_sds011 = 0; }
+if (isset($values["SDS_P1"]) && isset($values["SDS_P2"])) { $has_sds011 = 1; } else { $has_sds011 = 0; }
+if (isset($values["temperature"]) && isset($values["humidity"])) { $has_dht = 1; } else { $has_dht = 0; }
+if (isset($values["temperature"]) && isset($values["pressure"])) { $has_bmp = 1; } else { $has_bmp = 0; }
+
+// print transmitted values
 print "Sensor: ".$headers['Sensor']."\r\n";
 print "P1: ".$values["P1"]."\r\n";
 print "P2: ".$values["P2"]."\r\n";
-print "temp: ".$values["temp"]."\r\n";
+print "temp.: ".$values["temp"]."\r\n";
 print "humidity: ".$values["humidity"]."\r\n";
+print "pressure: ".$values["pressure"]."\r\n";
 print "SDS_P1: ".$values["SDS_P1"]."\r\n";
 print "SDS_P2: ".$values["SDS_P2"]."\r\n";
 print "Samples: ".$values["samples"]."\r\n";
 print "Min cycle: ".$values["min_micro"]."\r\n";
 print "Max cycle: ".$values["max_micro"]."\r\n";
 
+// check if data dir exists, create if not
 if (!file_exists('data')) {
 	mkdir('data', 0755, true);
 }
 
-$datafile = "data/data-".$headers['Sensor'].".rrd";
+// create update string P1,P2 for ppd42ns
 
-if (!file_exists($datafile)) {
-	$opts = array(
-		"--step", "30", "--start", time(),
-		"DS:PMone:GAUGE:45:U:U",
-		"DS:PMtwo:GAUGE:45:U:U",
-		"RRA:AVERAGE:0.5:10:288",
-		"RRA:AVERAGE:0.5:120:168",
-		"RRA:AVERAGE:0.5:2880:365",
-	);
-	$ret = rrd_create($datafile, $opts);
-	if (! $ret) {
-		$err = rrd_error();
-		echo "<b>Creation error: </b> $err\n";
+if ($has_ppd42ns) {
+	$update_string_ppd42ns = time().":";
+	if ($values["ratioP1"] < 15) { $update_string_ppd42ns .= $values["P1"]; }
+	$update_string_ppd42ns .= ":";
+	if ($values["ratioP2"] < 15) { $update_string_ppd42ns .= $values["P2"]; }
+	print $update_string_ppd42ns."\r\n";
+}
+
+if ($has_sds011) {
+	$update_string_sds011 = time().":";
+	if (isset($values["SDS_P1"])) {
+		$update_string_sds011 .= $values["SDS_P1"];
+		$update_string_sds011 .= $values["SDS_P2"];
+	} else {
+		$update_string_sds011 .= $values["P1"];
+		$update_string_sds011 .= $values["P2"];
 	}
+	print $update_string_sds011."\r\n";
 }
 
-$update_string = time().":";
-if ($values["ratioP1"] < 15) { $update_string .= $values["P1"]; }
-$update_string .= ":";
-if ($values["ratioP2"] < 15) { $update_string .= $values["P2"]; }
-
-print $update_string."\r\n";
-$ret = rrd_update($datafile,array($update_string));
-if (! $ret) {
-	$err = rrd_error();
-	echo "<b>Update error: </b> $err\n";
+if ($has_dht) {
+	$update_string_dht = time().":";
+	$update_string_dht .= $values["temperature"];
+	$update_string_dht .= $values["humidity"];
+	print $update_string_dht."\r\n";
 }
 
-$datafile = "data/data-".$headers['Sensor']."-highres.rrd";
+// update ppd42ns rrd file
+if ($has_ppd42ns) {
 
-if (!file_exists($datafile)) {
-	$opts = array(
-		"--step", "30", "--start", time(),
-		"DS:PMone:GAUGE:55:U:U",
-		"DS:PMtwo:GAUGE:55:U:U",
-		"RRA:AVERAGE:0.5:1:25920",
-		"RRA:AVERAGE:0.5:30:672",
-		"RRA:AVERAGE:0.5:720:1460",
-	);
-	$ret = rrd_create($datafile, $opts);
-	if (! $ret) {
-		$err = rrd_error();
-		echo "<b>Creation error: </b> $err\n";
-	}
-}
-
-$ret = rrd_update($datafile,array($update_string));
-if (! $ret) {
-	$err = rrd_error();
-	echo "<b>Update error: </b> $err\n";
-}
-
-if (isset($values["SDS_P1"]) || isset($values["SDS_P2"])) {
-
-	$update_string = time().":".$values["SDS_P1"].":".$values["SDS_P2"];
-
-	$datafile = "data/data-SDS".$headers['Sensor']."-highres.rrd";
+	$datafile = "data/data-".$headers['Sensor']."-ppd42ns-highres.rrd";
 
 	if (!file_exists($datafile)) {
 		$opts = array(
 			"--step", "30", "--start", time(),
 			"DS:PMone:GAUGE:55:U:U",
 			"DS:PMtwo:GAUGE:55:U:U",
-			"RRA:AVERAGE:0.5:1:2880",
+			"RRA:AVERAGE:0.5:1:25920",
 			"RRA:AVERAGE:0.5:30:672",
 			"RRA:AVERAGE:0.5:720:1460",
 		);
@@ -103,13 +88,70 @@ if (isset($values["SDS_P1"]) || isset($values["SDS_P2"])) {
 		}
 	}
 
-	$ret = rrd_update($datafile,array($update_string));
+	$ret = rrd_update($datafile,array($update_string_ppd42ns));
 	if (! $ret) {
 		$err = rrd_error();
 		echo "<b>Update error: </b> $err\n";
 	}
 }
 
+// update sds011 rrd file
+if ($has_sds011) {
+
+	$datafile = "data/data-".$headers['Sensor']."-sds011-highres.rrd";
+
+	if (!file_exists($datafile)) {
+		$opts = array(
+			"--step", "30", "--start", time(),
+			"DS:PMone:GAUGE:55:U:U",
+			"DS:PMtwo:GAUGE:55:U:U",
+			"RRA:AVERAGE:0.5:1:25920",
+			"RRA:AVERAGE:0.5:30:672",
+			"RRA:AVERAGE:0.5:720:1460",
+		);
+		$ret = rrd_create($datafile, $opts);
+		if (! $ret) {
+			$err = rrd_error();
+			echo "<b>Creation error: </b> $err\n";
+		}
+	}
+
+	$ret = rrd_update($datafile,array($update_string_sds011));
+	if (! $ret) {
+		$err = rrd_error();
+		echo "<b>Update error: </b> $err\n";
+	}
+}
+
+// update dht rrd file
+if ($has_dht) {
+
+	$datafile = "data/data-".$headers['Sensor']."-dht-highres.rrd";
+
+	if (!file_exists($datafile)) {
+		$opts = array(
+			"--step", "30", "--start", time(),
+			"DS:temperature:GAUGE:55:U:U",
+			"DS:humidity:GAUGE:55:U:U",
+			"RRA:AVERAGE:0.5:1:25920",
+			"RRA:AVERAGE:0.5:30:672",
+			"RRA:AVERAGE:0.5:720:1460",
+		);
+		$ret = rrd_create($datafile, $opts);
+		if (! $ret) {
+			$err = rrd_error();
+			echo "<b>Creation error: </b> $err\n";
+		}
+	}
+
+	$ret = rrd_update($datafile,array($update_string_dht));
+	if (! $ret) {
+		$err = rrd_error();
+		echo "<b>Update error: </b> $err\n";
+	}
+}
+
+// save max, min sample times
 if (isset($values["min_micro"]) || isset($values["max_micro"])) {
 
 	$update_string = time().":".$values["min_micro"].":".$values["max_micro"];
@@ -142,15 +184,16 @@ if (isset($values["min_micro"]) || isset($values["max_micro"])) {
 	}
 }
 
+// save data values to CSV (one per day)
 $datafile = "data/data-".$headers['Sensor']."-".$today.".csv";
 
 if (!file_exists($datafile)) {
 	$outfile = fopen($datafile,"a");
-	fwrite($outfile,"Time;durP1;ratioP1;P1;durP2;ratioP2;P2;SDS_P1;SDS_P2;Temp;Humidity;Samples;Min_cycle;Max_cycle\n");
+	fwrite($outfile,"Time;durP1;ratioP1;P1;durP2;ratioP2;P2;SDS_P1;SDS_P2;Temp;Humidity;Pressure;Samples;Min_cycle;Max_cycle\n");
 	fclose($outfile);
 }
 $outfile = fopen($datafile,"a");
-fwrite($outfile,$now.";".$values["durP1"].";".$values["ratioP1"].";".$values["P1"].";".$values["durP2"].";".$values["ratioP2"].";".$values["P2"].";".$values["SDS_P1"].";".$values["SDS_P2"].";".$values["temp"].";".$values["humidity"].";".$values["samples"].";".$values["min_micro"].";".$values["max_micro"]."\n");
+fwrite($outfile,$now.";".$values["durP1"].";".$values["ratioP1"].";".$values["P1"].";".$values["durP2"].";".$values["ratioP2"].";".$values["P2"].";".$values["SDS_P1"].";".$values["SDS_P2"].";".$values["temp"].";".$values["humidity"].";".$values["pressure"].";".$values["samples"].";".$values["min_micro"].";".$values["max_micro"]."\n");
 fclose($outfile);
 
 ?>
--- graph.php
+++ graph.php
@@ -21,34 +21,34 @@ if ($_GET['sensor']) {
 	create_graph("images/sensor-".$sensor."-25-month.png", "-1m", "Sensor data over one month",$sensor,25);
 	create_graph("images/sensor-".$sensor."-1-year.png", "-1y", "Sensor data over one year",$sensor,1);
 	create_graph("images/sensor-".$sensor."-25-year.png", "-1y", "Sensor data over one year",$sensor,25);
-	create_graph("images/sensor-".$sensor."-1-floating.png", "-8d", "Floating 24h average over one day",$sensor,101);
-	create_graph("images/sensor-".$sensor."-25-floating.png", "-8d", "Floating 24h average over one day",$sensor,125);
+	create_graph("images/sensor-".$sensor."-1-floating.png", "-8d", "Floating 24h average over 7 days",$sensor,101);
+	create_graph("images/sensor-".$sensor."-25-floating.png", "-8d", "Floating 24h average over 7 days",$sensor,125);
 
 	echo "<table>";
 	echo "<tr><td>";
-	echo "<img src='images/sensor-".$sensor."-1-day.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-1-day.png' alt='Sensor data over one day'>";
 	echo "</td><td>";
-	echo "<img src='images/sensor-".$sensor."-25-day.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-25-day.png' alt='Sensor data over one day'>";
 	echo "</td></tr>";
 	echo "<tr><td>";
-	echo "<img src='images/sensor-".$sensor."-1-week.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-1-week.png' alt='Sensor data over one week'>";
 	echo "</td><td>";
-	echo "<img src='images/sensor-".$sensor."-25-week.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-25-week.png' alt='Sensor data over one week'>";
 	echo "</td></tr>";
 	echo "<tr><td>";
-	echo "<img src='images/sensor-".$sensor."-1-month.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-1-month.png' alt='Sensor data over one month'>";
 	echo "</td><td>";
-	echo "<img src='images/sensor-".$sensor."-25-month.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-25-month.png' alt='Sensor data over one month'>";
 	echo "</td></tr>";
 	echo "<tr><td>";
-	echo "<img src='images/sensor-".$sensor."-1-year.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-1-year.png' alt='Sensor data over one year'>";
 	echo "</td><td>";
-	echo "<img src='images/sensor-".$sensor."-25-year.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-25-year.png' alt='Sensor data over one year'>";
 	echo "</td></tr>";
 	echo "<tr><td>";
-	echo "<img src='images/sensor-".$sensor."-1-floating.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-1-floating.png' alt='Floating 24h average over 7 days'>";
 	echo "</td><td>";
-	echo "<img src='images/sensor-".$sensor."-25-floating.png' alt='Generated RRD image'>";
+	echo "<img src='images/sensor-".$sensor."-25-floating.png' alt='Floating 24h average over 7 days'>";
 	echo "</td></tr>";
 	echo "</table>";
 	exit;
@@ -60,9 +60,8 @@ if ($_GET['sensor']) {
 		echo "<a href='graph.php?sensor=".$sensor."'>".$sensor."</a><br />\n";
 		create_graph("images/sensor-".$sensor."-1-day.png", "-1d", "Sensor data over one day (high res)",$sensor,1);
 		create_graph("images/sensor-".$sensor."-25-day.png", "-1d", "Sensor data over one day (high res)",$sensor,25);
-		echo "<img src='images/sensor-".$sensor."-1-day.png' alt='Generated RRD image'>
-<img src='images/sensor-".$sensor."-25-day.png' alt='Generated RRD image'>
-<br /><br /><br />";
+		echo "<img src='images/sensor-".$sensor."-1-day.png' alt='Sensor data over one day'>
+		<img src='images/sensor-".$sensor."-25-day.png' alt='Sensor data over one day'><br /><br /><br />";
 	}
 
 } elseif (isset($_GET['showweek'])) {
@@ -72,7 +71,8 @@ if ($_GET['sensor']) {
 		echo "<a href='graph.php?sensor=".$sensor."'>".$sensor."</a><br />\n";
 		create_graph("images/sensor-".$sensor."-1-week.png", "-1w", "Sensor data over one week",$sensor,1);
 		create_graph("images/sensor-".$sensor."-25-week.png", "-1w", "Sensor data over one week",$sensor,25);
-		echo "<img src='images/sensor-".$sensor."-1-week.png' alt='Generated RRD image'> <img src='images/sensor-".$sensor."-25-week.png' alt='Generated RRD image'><br /><br /><br />";
+		echo "<img src='images/sensor-".$sensor."-1-week.png' alt='Sensor data over one week'>
+		<img src='images/sensor-".$sensor."-25-week.png' alt='Sensor data over one week'><br /><br /><br />";
 	}
 
 } elseif (isset($_GET['showmonth'])) {
@@ -82,7 +82,8 @@ if ($_GET['sensor']) {
 		echo "<a href='graph.php?sensor=".$sensor."'>".$sensor."</a><br />\n";
 		create_graph("images/sensor-".$sensor."-1-month.png", "-1m", "Sensor data over one month",$sensor,1);
 		create_graph("images/sensor-".$sensor."-25-month.png", "-1m", "Sensor data over one month",$sensor,25);
-		echo "<img src='images/sensor-".$sensor."-1-month.png' alt='Generated RRD image'> <img src='images/sensor-".$sensor."-25-month.png' alt='Generated RRD image'><br /><br /><br />";
+		echo "<img src='images/sensor-".$sensor."-1-month.png' alt='Sensor data over one month'>
+		<img src='images/sensor-".$sensor."-25-month.png' alt='Sensor data over one month'><br /><br /><br />";
 	}
 
 } elseif (isset($_GET['showfloat'])) {
@@ -90,9 +91,10 @@ if ($_GET['sensor']) {
 	foreach (glob("data/*-highres.rrd") as $filename) {
 		$sensor = substr($filename,10,-12);
 		echo "<a href='graph.php?sensor=".$sensor."'>".$sensor."</a><br />\n";
-		create_graph("images/sensor-".$sensor."-1-floating.png", "-8d", "Floating 24h average over one day",$sensor,101);
-		create_graph("images/sensor-".$sensor."-25-floating.png", "-8d", "Floating 24h average over one day",$sensor,125);
-		echo "<img src='images/sensor-".$sensor."-1-floating.png' alt='Generated RRD image'> <img src='images/sensor-".$sensor."-25-floating.png' alt='Generated RRD image'><br /><br /><br />";
+		create_graph("images/sensor-".$sensor."-1-floating.png", "-8d", "Floating 24h average over 7 days",$sensor,101);
+		create_graph("images/sensor-".$sensor."-25-floating.png", "-8d", "Floating 24h average over 7 days",$sensor,125);
+		echo "<img src='images/sensor-".$sensor."-1-floating.png' alt='Floating 24h average over 7 days'>
+		<img src='images/sensor-".$sensor."-25-floating.png' alt='Floating 24h average over 7 days'><br /><br /><br />";
 	}
 
 } else {
@@ -106,10 +108,7 @@ if ($_GET['sensor']) {
 
 function create_graph($output, $start, $title, $sensor, $option_nr) {
 	$options = array(
-//		"--slope-mode",
 		"--start", $start,
-//		"--start", "e".$start,
-//		"--end", "19:00 20160101",
 		"--title=$title",
 		"--vertical-label=Partikel / Liter",
 		"--lower=0",
@@ -119,65 +118,21 @@ function create_graph($output, $start, $title, $sensor, $option_nr) {
 
 	if ($option_nr === 1) {
 		array_push($options,"DEF:PMone=data/data-".$sensor."-highres.rrd:PMone:AVERAGE:step=30");
-		array_push($options,"CDEF:v1=PMone");
-		array_push($options,"CDEF:v2=PREV(v1)");
-		array_push($options,"CDEF:v3=PREV(v2)");
-		array_push($options,"CDEF:v4=PREV(v3)");
-		array_push($options,"CDEF:v5=PREV(v4)");
-		array_push($options,"CDEF:avg1PMone=v1,v2,v3,v4,v5,5,SORT,POP,POP,3,REV,POP,POP");
-//		array_push($options,"CDEF:avg2PMone=v1,v2,v3,v4,v5,5,SORT,POP,4,REV,POP,3,AVG,1000,*,283,/");
-//		array_push($options,"CDEF:avg2PMone=v1,1000,*,283,/");
-//		array_push($options,"CDEF:avg2PMone=v1,v2,v3,v4,v5,5,SORT,POP,POP,3,REV,POP,POP"); // Median over 5 values
-		array_push($options,"CDEF:avg2PMone=PMone,300,TRENDNAN"); // 24h average
-//		array_push($options,"LINE1:PMone#FFFF00:'PM1'");
-//		array_push($options,"LINE1:avg1PMone#FF0000:'PM1'");
-		array_push($options,"LINE1:avg2PMone#FF0000:'PM1'");
+		array_push($options,"CDEF:avgPMone=PMone,300,TRENDNAN"); // 5 min (300 sec) floating average
+		array_push($options,"LINE1:avgPMone#FF0000:'PM1'");
 	} else if ($option_nr === 25) {
 		array_push($options,"DEF:PMtwo=data/data-".$sensor."-highres.rrd:PMtwo:AVERAGE:step=30");
-		array_push($options,"CDEF:v1=PMtwo");
-		array_push($options,"CDEF:v2=PREV(v1)");
-		array_push($options,"CDEF:v3=PREV(v2)");
-		array_push($options,"CDEF:v4=PREV(v3)");
-		array_push($options,"CDEF:v5=PREV(v4)");
-		array_push($options,"CDEF:avg1PMtwo=v1,v2,v3,v4,v5,5,SORT,POP,POP,3,REV,POP,POP");
-//		array_push($options,"CDEF:avg2PMtwo=v1,v2,v3,v4,v5,5,SORT,POP,4,REV,POP,3,AVG,1000,*,283,/");
-//		array_push($options,"CDEF:avg2PMtwo=v1,1000,*,283,/");
-//		array_push($options,"CDEF:avg2PMtwo=v1,v2,v3,v4,v5,5,SORT,POP,POP,3,REV,POP,POP"); // Median over 5 values
-		array_push($options,"CDEF:avg2PMtwo=PMtwo,300,TRENDNAN"); // 24h average
-//		array_push($options,"LINE1:PMtwo#FFFF00:'PM2.5'");
-//		array_push($options,"LINE1:avg1PMtwo#FF0000:'PM2.5'");
-		array_push($options,"LINE1:avg2PMtwo#0000FF:'PM2.5'");
+		array_push($options,"CDEF:avgPMtwo=PMtwo,300,TRENDNAN"); // 5 min (300 sec) floating average
+		array_push($options,"LINE1:avgPMtwo#0000FF:'PM2.5'");
 	} else if ($option_nr === 101) {
 		array_push($options,"DEF:PMone=data/data-".$sensor."-highres.rrd:PMone:AVERAGE:step=30");
-		array_push($options,"CDEF:v1=PMone");
-		array_push($options,"CDEF:v2=PREV(v1)");
-		array_push($options,"CDEF:v3=PREV(v2)");
-		array_push($options,"CDEF:v4=PREV(v3)");
-		array_push($options,"CDEF:v5=PREV(v4)");
-//		array_push($options,"CDEF:avg1PMone=v1,v2,v3,v4,v5,5,SORT,POP,POP,3,REV,POP,POP");
-		array_push($options,"CDEF:avg1PMone=v1,v2,v3,3,SORT,POP,2,REV,POP");
-		array_push($options,"CDEF:avg2PMone=PMone,86400,TRENDNAN"); // 24h average
-		array_push($options,"LINE1:avg2PMone#FF0000:'PM1'");
+		array_push($options,"CDEF:avgPMone=PMone,86400,TRENDNAN"); // 24h (24 * 60 * 60 = 86400 sec) floating average
+		array_push($options,"LINE1:avgPMone#FF0000:'PM1'");
 	} else if ($option_nr === 125) {
 		array_push($options,"DEF:PMtwo=data/data-".$sensor."-highres.rrd:PMtwo:AVERAGE:step=30");
-		array_push($options,"CDEF:v1=PMtwo");
-		array_push($options,"CDEF:v2=PREV(v1)");
-		array_push($options,"CDEF:v3=PREV(v2)");
-		array_push($options,"CDEF:v4=PREV(v3)");
-		array_push($options,"CDEF:v5=PREV(v4)");
-		array_push($options,"CDEF:avg1PMtwo=v1,v2,v3,3,SORT,POP,2,REV,POP");
-		array_push($options,"CDEF:avg2PMtwo=PMtwo,86400,TRENDNAN"); // 24h average
-		array_push($options,"LINE1:avg2PMtwo#0000FF:'PM2.5'");
+		array_push($options,"CDEF:avgPMtwo=PMtwo,86400,TRENDNAN"); // 24h (24 * 60 * 60 = 86400 sec) floating average
+		array_push($options,"LINE1:avgPMtwo#0000FF:'PM2.5'");
 	}
-
-//	if ($start === '-1d') {
-//		array_push($options,"--rigid");
-//		if ($option_nr == 1) {
-//			array_push($options,"--upper-limit=150000");
-//		} else {
-//			array_push($options,"--upper-limit=5000");
-//		} 
-//	}
 
 	$ret = rrd_graph($output, $options);
 
