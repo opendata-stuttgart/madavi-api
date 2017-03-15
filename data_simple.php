c897d63ce5e51bc31a1528c78325b1f840c7e16d
Rajko Zschiegner <ricki_z@gmx.de>
2017-03-15 23:51:21 +0000

    adding data_simple.php to save only csv

---
+++ data_simple.php
@@ -0,0 +1,62 @@
+<?php
+
+if (isset($_SERVER['HTTP_SENSOR'])) $headers['Sensor'] = $_SERVER['HTTP_SENSOR'];
+if (isset($_SERVER['HTTP_X_SENSOR']))$headers['Sensor'] = $_SERVER['HTTP_X_SENSOR'];
+
+$json = file_get_contents('php://input');
+
+$results = json_decode($json,true);
+
+header_remove();
+
+$now = gmstrftime("%Y/%m/%d %H:%M:%S");
+$today = gmstrftime("%Y-%m-%d");
+
+// copy sensor data values to values array
+foreach ($results["sensordatavalues"] as $sensordatavalues) {
+	$values[$sensordatavalues["value_type"]] = $sensordatavalues["value"];
+}
+
+// print transmitted values
+echo "Sensor: ".$headers['Sensor']."\r\n";
+
+// check if data dir exists, create if not
+if (!file_exists('data')) {
+	mkdir('data', 0755, true);
+}
+
+// save data values to CSV (one per day)
+$datafile = "data/data-".$headers['Sensor']."-".$today.".csv";
+
+if (!file_exists($datafile)) {
+	$outfile = fopen($datafile,"a");
+	fwrite($outfile,"Time;durP1;ratioP1;P1;durP2;ratioP2;P2;SDS_P1;SDS_P2;Temp;Humidity;BMP_temperature;BMP_pressure;BME280_temperature;BME280_humidity;BME280_pressure;Samples;Min_cycle;Max_cycle;Signal\n");
+	fclose($outfile);
+}
+
+if (! isset($values["durP1"])) { $values["durP1"] = ""; }
+if (! isset($values["ratioP1"])) { $values["ratioP1"] = ""; }
+if (! isset($values["P1"])) { $values["P1"] = ""; }
+if (! isset($values["durP2"])) { $values["durP2"] = ""; }
+if (! isset($values["ratioP2"])) { $values["ratioP2"] = ""; }
+if (! isset($values["P2"])) { $values["P2"] = ""; }
+if (! isset($values["SDS_P1"])) { $values["SDS_P1"] = ""; }
+if (! isset($values["SDS_P2"])) { $values["SDS_P2"] = ""; }
+if (! isset($values["temperature"])) { $values["temperature"] = ""; }
+if (! isset($values["humidity"])) { $values["humidity"] = ""; }
+if (! isset($values["BMP_temperature"])) { $values["BMP_temperature"] = ""; }
+if (! isset($values["BMP_pressure"])) { $values["BMP_pressure"] = ""; }
+if (! isset($values["BME280_temperature"])) { $values["BME280_temperature"] = ""; }
+if (! isset($values["BME280_humidity"])) { $values["BME280_humidity"] = ""; }
+if (! isset($values["BME280_pressure"])) { $values["BME280_pressure"] = ""; }
+if (! isset($values["samples"])) { $values["samples"] = ""; }
+if (! isset($values["min_micro"])) { $values["min_micro"] = ""; }
+if (! isset($values["max_micro"])) { $values["max_micro"] = ""; }
+if (! isset($values["signal"])) { $values["signal"] = ""; } else { $values["signal"] = substr($values["signal"],0,-4); }
+
+$outfile = fopen($datafile,"a");
+fwrite($outfile,$now.";".$values["durP1"].";".$values["ratioP1"].";".$values["P1"].";".$values["durP2"].";".$values["ratioP2"].";".$values["P2"].";".$values["SDS_P1"].";".$values["SDS_P2"].";".$values["temperature"].";".$values["humidity"].";".$values["BMP_temperature"].";".$values["BMP_pressure"].";".$values["BME280_temperature"].";".$values["BME280_humidity"].";".$values["BME280_pressure"].";".$values["samples"].";".$values["min_micro"].";".$values["max_micro"].";".$values["signal"]."\n");
+fclose($outfile);
+
+?>
+ok
